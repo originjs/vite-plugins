3655bf8cf44b05be5f9f524ffa5cc3740f0c0eae
50 <jiawulin@vip.qq.com>
2021-06-08 20:13:12 +0800

    add vite-plugin-commonjs and adjust the catalog

---
+++ packages/vite-plugin-commonjs/README.md
@@ -0,0 +1,29 @@
+# vite-plugin-commonjs
+
+Support commonJS to esm in `vite`
+
+## Install
+```shell
+npm install @originjs/vite-plugin-commonjs --save-dev
+```
+or
+```shell
+yarn add @originjs/vite-plugin-commonjs --dev
+```
+
+## Usage
+```js
+import commonjs from '@originjs/vite-plugin-commonjs'
+
+export default {
+    plugins: [
+        commonjs()
+    ]
+}
+```
+
+### Options
+
+- `exclude: string[]`
+  Dependencies to exclude from transform.
+  
---
+++ packages/vite-plugin-commonjs/src/index.ts
@@ -0,0 +1,71 @@
+import { transformSync, TransformResult } from "esbuild";
+import { Plugin } from "vite";
+import { sep } from "path";
+
+const commonJSRegex: RegExp = /\b(module\.exports|exports\.\w+|exports\s*=\s*)/;
+const requireRegex: RegExp = /require\((.*)\)/g;
+const IMPORT_STRING_PREFIX: String = "__require_for_vite";
+
+type PluginOptions = {
+  include?: string | RegExp | string[] | RegExp[] | undefined;
+  exclude?: string | RegExp | string[] | RegExp[] | undefined;
+};
+
+export default (options: PluginOptions): Plugin => {
+  return {
+    name: "originjs:commonjs",
+    apply: "serve",
+    transform(code: string, id: string): TransformResult {
+      if (id.indexOf("/node_modules/.vite/") > 0) {
+        return null;
+      }
+
+      const requireMatches = code.matchAll(requireRegex);
+      let importsString = "";
+      let packageName = "";
+      for (let item of requireMatches) {
+        packageName = `${IMPORT_STRING_PREFIX}_${randomString(6)}`;
+        importsString += `import * as ${packageName} from ${item[1]};\n`;
+        code = code.replace(item[0], packageName);
+      }
+
+      if (importsString) {
+        code = importsString + code;
+      }
+
+      if (isCommonJS(code)) {
+        return transformSync(code, { format: "esm" });
+      }
+
+      if (importsString) {
+        return {
+          code,
+          map: null,
+          warnings: null,
+        };
+      }
+
+      return null;
+    },
+  };
+};
+
+function isCommonJS(code: string): Boolean {
+  return commonJSRegex.test(code);
+}
+
+function randomString(length: number): string {
+  const code: string =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  let result: string = "";
+  for (let index = 0; index < length; index++) {
+    result += code[Math.floor(Math.random() * String.length)];
+  }
+  return result;
+}
+
+function idFilter(id: string): boolean {
+  if (typeof id !== "string") return false;
+
+  let paths = id.split(sep).join("/");
+}
