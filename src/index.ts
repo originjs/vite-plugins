6d288690b435d243feef40b4e952db17e9aea477
PeterAlfredLee <peteralfredlee@gmail.com>
2021-06-08 11:28:55 +0800

    implemention require context

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ type PluginOptions = {
 
 const DEFAULT_REGEXP: RegExp = /\.(json|js)$/
 const DEFAULT_PROJECT_BASE_PATH: string = process.cwd()
+const requireContextStringPrefix: string = '__require_context_for_vite'
 
 export default (options: PluginOptions = {}): Plugin => {
     return {
@@ -35,7 +36,7 @@ export default (options: PluginOptions = {}): Plugin => {
             }
 
             let transformedCode: string = code
-            let importsStrings: string = ''
+            let addedCode: string = ''
 
             requireContextMatches.forEach((requireContextMatch, index) => {
                 const params: string[] = requireContextMatch[1].split(',')
@@ -43,7 +44,7 @@ export default (options: PluginOptions = {}): Plugin => {
                 const recursive: string = params[1] || ''
                 const regExp: string = params[2] || ''
 
-                const { importsString, requireContextString } = transformRequireContext(
+                const { importsString, key2FilesMapString, contextFunctionString, requireContextFunctionName} = transformRequireContext(
                     eval(directory),
                     eval(recursive),
                     eval(regExp) || options.defaultRegExp,
@@ -52,14 +53,14 @@ export default (options: PluginOptions = {}): Plugin => {
                     index
                 )
 
-                importsStrings += importsString + '\n'
+                addedCode += importsString + key2FilesMapString + contextFunctionString
                 transformedCode = transformedCode.replace(
                     requireContextMatch[0],
-                    requireContextString
+                    requireContextFunctionName
                 )
             })
 
-            transformedCode = importsStrings + transformedCode
+            transformedCode = addedCode + transformedCode
             return { code: transformedCode }
         },
     }
@@ -73,7 +74,6 @@ function transformRequireContext(
     projectBasePath: string = DEFAULT_PROJECT_BASE_PATH,
     matchIndex: number
 ) {
-    const importStringPrefix: string = '__require_context_for_vite'
     let basePath: string
 
     switch (directory[0]) {
@@ -106,20 +106,112 @@ function transformRequireContext(
     })
 
     const keys: string[] = absolutePaths.map((absolutePath) => {
-        return absolutePath.slice(basePath.length + 1)
+        return './' + absolutePath.slice(basePath.length + 1)
     })
 
-    let importsString: string = ''
-    let requireContextString: string = '{'
-    keys.forEach((key, index) => {
-        const importEntry:string = `${importStringPrefix}_${matchIndex}_${index}`
-        importsString += `import * as ${importEntry} from "${importedFiles[index]}";`
-        requireContextString += ` ${JSON.stringify(key)} : ${importEntry},`
-    })
-    requireContextString = requireContextString.substring(0, requireContextString.length - 1) + '}'
+    const requireContextMapName = `${requireContextStringPrefix}_map_${matchIndex}`
+    const requireContextFunctionName = `${requireContextStringPrefix}_function_${matchIndex}`
+
+    const key2FilesMap = generateKey2FilesMap(keys, importedFiles, matchIndex)
+    const importsString = generateImportsString(keys, importedFiles, matchIndex)
+    const key2FilesMapString = generateKey2FilesMapString(key2FilesMap, requireContextMapName)
+    const contextFunctionString = generateContextFunctionString(requireContextFunctionName, requireContextMapName, matchIndex)
+
+    // let requireContextString: string = '{'
+    // keys.forEach((key, index) => {
+    //     const importEntry:string = `${importStringPrefix}_${matchIndex}_${index}`
+    //     importsString += `import * as ${importEntry} from "${importedFiles[index]}";`
+    //     requireContextString += ` ${JSON.stringify(key)} : ${importEntry},`
+    // })
+    // requireContextString = requireContextString.substring(0, requireContextString.length - 1) + '}'
 
     return {
         importsString,
-        requireContextString
+        key2FilesMapString,
+        contextFunctionString,
+        requireContextFunctionName
     }
+}
+
+function generateKey2FilesMap(
+    keys: string[],
+    importedFiles: string[],
+    matchIndex: number
+): object {
+    let key2FilesMap: object = {}
+    keys.forEach((key, index) => {
+        const importEntry:string = `${requireContextStringPrefix}_${matchIndex}_${index}`
+        key2FilesMap[key] = {
+            'importEntry': importEntry,
+            'filePath': './' + importedFiles[index]
+        }
+    })
+
+    return key2FilesMap
+}
+
+function generateImportsString(
+    keys: string[],
+    importedFiles: string[],
+    matchIndex: number
+): string {
+    let importsString: string = ''
+    for (let index = 0; index < keys.length; index++) {
+        const importEntry:string = `${requireContextStringPrefix}_${matchIndex}_${index}`
+        importsString += `import * as ${importEntry} from "${importedFiles[index]}";`
+    }
+    importsString += '\n'
+
+    return importsString
+}
+
+function generateKey2FilesMapString (
+    key2FilesMap: object,
+    requireContextMapName: string
+): string {
+    let key2FilesMapString = `var ${requireContextMapName} = {\n`
+    Object.keys(key2FilesMap).forEach((key) => {
+        key2FilesMapString += `"${key}" : ${key2FilesMap[key].importEntry},\n`
+    })
+    key2FilesMapString = key2FilesMapString.substring(0, key2FilesMapString.length - 2) + '\n}'
+
+    return key2FilesMapString
+}
+
+function generateContextFunctionString(
+    requireContextFunctionName: string,
+    requireContextMapName: string,
+    matchIndex: number
+): string {
+    const requireContextResolveFunctionName = `${requireContextFunctionName}_resolve`
+    const requireContextKeysFunctionName = `${requireContextFunctionName}_keys`
+    // webpackContext(req)
+    let contextFunctionString = `function ${requireContextFunctionName}(req) {\n
+        var id = ${requireContextResolveFunctionName}(req);\n
+        return ${requireContextMapName}[req];\n
+    }\n`
+
+    // webpackContextResolve(req)
+    contextFunctionString += `function ${requireContextResolveFunctionName}(req) {\n
+        if (req in ${requireContextMapName}) {\n
+            return ${requireContextMapName}[req];\n
+        }\n
+        var e = new Error("Cannot find module '" + req + "'");\n
+        e.code = 'MODULE_NOT_FOUND';\n
+        throw e;\n
+    }\n`
+
+    // webpackConext.keys
+    contextFunctionString += `${requireContextFunctionName}.keys = function ${requireContextKeysFunctionName}() {\n
+        return Object.keys(${requireContextMapName});\n
+    }\n`
+
+    // webpackContext.resolve
+    contextFunctionString += `${requireContextFunctionName}.resolve = ${requireContextResolveFunctionName}\n`
+
+    // webpackContext.id
+    // TODO: not implemented
+    contextFunctionString += `${requireContextFunctionName}.id = "id${matchIndex}"`
+
+    return contextFunctionString
 }
