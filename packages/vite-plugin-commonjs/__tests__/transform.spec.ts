9b8a7c78b9227b4378eec4fe5ce8c1d5cdbdbfb1
joey <flyfish.zy@gmail.com>
2021-06-15 15:26:34 +0800

    add test cases

---
+++ packages/vite-plugin-commonjs/__tests__/transform.spec.ts
@@ -0,0 +1,30 @@
+import { transformRequire, isCommonJS } from "../src/lib";
+
+test('transform require', () => {
+    //general require
+    let code = `require("react");`
+    let result = transformRequire(code, 'main.ts');
+    expect(result.code).toMatch(/import \* as .+ from "react";/);
+
+    //nested require
+    code = `_interopRequire(require('./DatePicker'));`
+    result = transformRequire(code, 'main.ts');
+    expect(result.code).toMatch(/import \* as .+ from '\.\/DatePicker';/);
+
+    code = `var obj = require('./module.js' );`
+    result = transformRequire(code, 'main.ts');
+    expect(result.code).toMatch(/import \* as .+ from '\.\/module.js';\nvar obj = /);
+
+    code = `_interopRequire(require('./DatePicker'));`
+    result = transformRequire(code, 'main.ts');
+    expect(result.code).toMatch(/import \* as .+ from '\.\/DatePicker';/);
+
+    code = `require('./module.js').message = "hello";`
+    result = transformRequire(code, 'main.ts');
+    expect(result.code).toMatch(/import \* as .+ from '\.\/module.js';\n.+\.message = "hello"/);
+});
+
+test('isCommonJS', () => {
+    expect(isCommonJS(`module.exports = {}`)).toBeTruthy();
+    expect(isCommonJS(`exports = { hello: false }`)).toBeTruthy();
+});
